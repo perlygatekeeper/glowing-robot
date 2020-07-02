b20d6a7a6961098fd6aa4cedcc6ed33a54f05155
Steven Parker <perlygatekeeper@gmail.com>
2020-07-02 14:16:23 -0400

    finished grandest staircase ever. started queue_to_do on google's foobar

--- google_test/grandest_staircase_EVER/solution_with_debug.py
+++ google_test/grandest_staircase_EVER/solution_with_debug.py
@@ -5,18 +5,23 @@ def solution(n):
     for i in range(1,20):
            triangle_numbers.append(int( i * (i+1) / 2 ))
     number = 0
+#   print(triangle_numbers)
     for i in range(0,201):
         if i in triangle_numbers:
             number += 1
         triangle_indices.append(number)
-#   triangle numbers and indices done.
+#   print(triangle_indices)
     staircases = {}
     for N in range(3,n+1):
+#       print("N is %d" % (N))
         staircases[N] = {}
         smallest_stairway_height = triangle_indices[N]
+#       print("smallest_stairway_height is %d" % (smallest_stairway_height))
         for stairway_height in range(smallest_stairway_height,N):
             # m is # of bricks left to build rest of stairway
             m = N - stairway_height
+#           print("stairway_height is %d"     % (stairway_height))
+#           print("bricks for rest is %d (m)" % (m))
             if stairway_height > smallest_stairway_height:
                 staircases[N][stairway_height] = staircases[N][stairway_height-1]
             else:
@@ -27,6 +32,10 @@ def solution(n):
                     staircases[N][stairway_height] += staircases[m][m-1]
             else:
                 staircases[N][stairway_height] += staircases[m][stairway_height-1]
+#       print(staircases)
     return staircases[n][n-1]
+            
+
+
 n = 200
 print("%3d: %7d" % (n, solution(n)) )
---
+++ google_test/queue_to_do/test_XOR.py
@@ -0,0 +1,37 @@
+
+# simple script to show XOR's of different ranges of integers
+
+checksum = 0
+
+n = 200001
+start = int(4*n + 3)
+start = int(4*n + 0)
+start = int(4*n + 1)
+start = int(4*n + 2)
+start = 254
+for i in range(start,start+8+1):
+    checksum ^= i
+    print("{0:4d}: {0:10b}  {1:4d}: {1:10b}".format(i,checksum))
+
+#for i in range(0,1000000000):
+#    checksum ^= i
+#   print("{0:4d}: {0:10b}  {1:4d}: {1:10b}".format(i,checksum))
+#print checksum
+
+
+# if line_length > 400
+#   if start is odd
+#     checksum = start
+#     line_length -= 1
+#   else
+#     checksum = 0
+#     start here
+#   if line_length % 4 == 0:
+#      done
+#   if line_length % 4 == 1:
+#      checksum ^= last_number
+#   if line_length % 4 == 2:
+#      checksum ^= 1
+#   if line_length % 4 == 3:
+#      checksum ^= last_number
+#      checksum ^= 1
