f60a8742c0e3b2c9e47aed438c3e423684bd3dd8
Steven Parker <perlygatekeeper@gmail.com>
2026-01-07 22:09:11 -0500

    added my looped corner ornamental border, just before break up into seperate files

--- Papers/paper_templates.py
+++ Papers/paper_templates.py
@@ -601,6 +601,75 @@ class OrnamentalBorder(PaperTemplate):
         svg += f'stroke="#cccccc" stroke-width="0.5" opacity="0.5"/>\n'
         
         return svg
+    
+    def _draw_looped_corner(self):
+        """Page with looped corner effect"""
+        svg = ''
+        
+        # Main page border
+        x = self.margin
+        y = self.margin
+        w = self.width - 2 * self.margin
+        h = self.height - 2 * self.margin
+        
+        # step size
+        step = 10
+        # Draw page outline
+        svg += f'  <rect x="{x}" y="{y}" width="{w}" height="{h}" '
+        svg += f'fill="none" stroke="#000000" stroke-width="{1.2*self.thickness}"/>\n'
+
+        x = x + step
+        y = y + step
+        w = w - 2 * step
+        h = h - 2 * step
+
+        # Start at top-left, go clockwise, complete loop at each corner
+        svg += f'  <path d="'
+        svg += f'M {x + 0 * step} {y + 1 * step} '
+        svg += f'L {x + 2 * step} {y + 1 * step} '
+        svg += f'L {x + 2 * step} {y + 2 * step} '
+        svg += f'L {x + 1 * step} {y + 2 * step} '
+        svg += f'L {x + 1 * step} {y + 0 * step} '
+        # On to top-right, continue clockwise, complete loop at each corner
+        svg += f'L {x + w - 1 * step} {y + 0 * step} '
+        svg += f'L {x + w - 1 * step} {y + 2 * step} '
+        svg += f'L {x + w - 2 * step} {y + 2 * step} '
+        svg += f'L {x + w - 2 * step} {y + 1 * step} '
+        svg += f'L {x + w - 0 * step} {y + 1 * step} '
+        # On to bottom-right, continue clockwise, complete loop at each corner
+        svg += f'L {x + w - 0 * step} {y + h - 1 * step} '
+        svg += f'L {x + w - 2 * step} {y + h - 1 * step} '
+        svg += f'L {x + w - 2 * step} {y + h - 2 * step} '
+        svg += f'L {x + w - 1 * step} {y + h - 2 * step} '
+        svg += f'L {x + w - 1 * step} {y + h - 0 * step} '
+        # On to bottom-left, go clockwise, complete loop at each corner
+        svg += f'L {x + 1 * step} {y + h - 0 * step} '
+        svg += f'L {x + 1 * step} {y + h - 2 * step} '
+        svg += f'L {x + 2 * step} {y + h - 2 * step} '
+        svg += f'L {x + 2 * step} {y + h - 1 * step} '
+        svg += f'L {x + 0 * step} {y + h - 1 * step} '
+        svg += f'L {x + 0 * step} {y + 1 * step} '
+        svg += f'Z" '  # Close path
+        svg += f'fill="none" stroke="#000000" stroke-width="{self.thickness * 0.5}"/>\n'
+
+        # Draw the inner-most almost-square
+        svg += f'  <path d="'
+        svg += f'M {x + 1 * step} {y + 3 * step} '
+        svg += f'L {x + 3 * step} {y + 3 * step} '
+        svg += f'L {x + 3 * step} {y + 1 * step} '
+        svg += f'L {x + w - 3 * step} {y + 1 * step} '
+        svg += f'L {x + w - 3 * step} {y + 3 * step} '
+        svg += f'L {x + w - 1 * step} {y + 3 * step} '
+        svg += f'L {x + w - 1 * step} {y + h - 3 * step} '
+        svg += f'L {x + w - 3 * step} {y + h - 3 * step} '
+        svg += f'L {x + w - 3 * step} {y + h - 1 * step} '
+        svg += f'L {x + 3 * step} {y + h - 1 * step} '
+        svg += f'L {x + 3 * step} {y + h - 3 * step} '
+        svg += f'L {x + 1 * step} {y + h - 3 * step} '
+        svg += f'L {x + 1 * step} {y + 3 * step} '
+        svg += f'Z" '
+        svg += f'fill="#FFFFFF" stroke="#000000" stroke-width="{self.thickness * 0.5}"/>\n'
+        return svg
 
 
 class WeeklyPlannerPaper(PaperTemplate):
